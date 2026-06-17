45b407f0f32fd1faf2cd7f588609e99d42fa4403
Jhos-130 <josuecamtor@gmail.com>
2026-06-17 11:53:39 -0600

    Guarda

--- models/libro.py
+++ models/libro.py
@@ -1,12 +1,12 @@
 class Libro:
 
     #Constructor
-    def __init__(self, id_libro, titulo, autor, isbn):
-        self.id_libro = id_libro
+    def __init__(self, id_libro, titulo, autor, isbn, disponible):
+        self.id = id
         self.titulo = titulo
         self.autor = autor
         self.isbn = isbn
-        self.disponible = True #Por defecto, el libro esta disponible
+        self.disponible = disponible #Por defecto, el libro esta disponible
 
     def prestar(self):
         if self.disponible:
@@ -18,4 +18,4 @@ class Libro:
         self.disponible = True
 
     def mostrar_info(self):
-        return f"Libro ID: {self.id_libro}, Titulo: {self.titulo}, Autor: {self.autor}, ISMN: {self.isbn}, Disponible: {'Si' if self.disponible else 'No'}"
+        return f"Libro ID: {self.id}, Titulo: {self.titulo}, Autor: {self.autor}, ISMN: {self.isbn}, Disponible: {'Si' if self.disponible else 'No'}"
